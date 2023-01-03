fe4c4fa4230a1bbe8a88f3293ed965fd854e5980
Docker <docker@example.com>
2023-01-03 13:42:22 +0000

    Delno rešena domača naloga

--- domaca-naloga/model.ml
+++ domaca-naloga/model.ml
@@ -1,6 +1,6 @@
 (* Pomožni tip, ki predstavlja mrežo *)
 
-type 'a grid = 'a Array.t Array.t
+type 'a grid = ('a Array.t) Array.t (*tabela tabel tipa a*)
 
 (* Funkcije za prikaz mreže.
    Te definiramo najprej, da si lahko z njimi pomagamo pri iskanju napak. *)
@@ -43,22 +43,24 @@ let print_grid string_of_cell grid =
 
 (* Funkcije za dostopanje do elementov mreže *)
 
-let get_row (grid : 'a grid) (row_ind : int) = failwith "TODO"
-
-let rows grid = failwith "TODO"
+let get_row (grid : 'a grid) (row_ind : int) = 
+  Array.copy grid.(row_ind)
+let rows grid = List.init 9 (get_row grid)
 
 let get_column (grid : 'a grid) (col_ind : int) =
   Array.init 9 (fun row_ind -> grid.(row_ind).(col_ind))
 
 let columns grid = List.init 9 (get_column grid)
 
-let get_box (grid : 'a grid) (box_ind : int) = failwith "TODO"
+let get_box (grid : 'a grid) (box_ind : int) =
+  Array.init 9 (fun ind -> grid.(3 * (box_ind / 3) + (ind / 3)).(3 * (box_ind mod 3) + (ind mod 3)))
 
-let boxes grid = failwith "TODO"
+let boxes grid = List.init 9 (get_box grid)
 
 (* Funkcije za ustvarjanje novih mrež *)
 
-let map_grid (f : 'a -> 'b) (grid : 'a grid) : 'b grid = failwith "TODO"
+let map_grid (f : 'a -> 'b) (grid : 'a grid) : 'b grid =
+  Array.init 9 (fun row_ind -> Array.init 9 (fun col_ind -> f (grid.(row_ind).(col_ind))))
 
 let copy_grid (grid : 'a grid) : 'a grid = map_grid (fun x -> x) grid
 
@@ -95,9 +97,10 @@ let grid_of_string cell_of_char str =
 
 (* Model za vhodne probleme *)
 
-type problem = { initial_grid : int option grid }
+type problem = { initial_grid : int option grid } 
 
-let print_problem problem : unit = failwith "TODO"
+let print_problem problem : unit =
+  print_grid (fun (element : int option) ->  match element with | None -> " " | Some element -> (string_of_int element)) problem
 
 let problem_of_string str =
   let cell_of_char = function
@@ -111,6 +114,69 @@ let problem_of_string str =
 
 type solution = int grid
 
-let print_solution solution = failwith "TODO"
+let print_solution solution = 
+  print_grid (fun a -> match a with |None -> " " |Some k -> string_of_int k ) solution
 
-let is_valid_solution problem solution = failwith "TODO"
+let is_valid_solution (problem: int option grid) (solution: int option grid) =
+  try
+    for i = 0 to 8 do
+      if not ((List.sort compare (Array.to_list (get_row solution i)) = [Some 1; Some 2; Some 3; Some 4; Some 5; Some 6; Some 7; Some 8; Some 9]) &&
+        (List.sort compare (Array.to_list (get_column solution i)) = [Some 1; Some 2; Some 3; Some 4; Some 5; Some 6; Some 7; Some 8; Some 9]) &&
+        (List.sort compare (Array.to_list (get_box solution i)) = [Some 1; Some 2; Some 3; Some 4; Some 5; Some 6; Some 7; Some 8; Some 9])) then raise Exit; 
+      for j = 0 to 8 do
+        if (not ((problem.(i).(j) = None))) && (not (Option.get problem.(i).(j) = Option.get solution.(i).(j))) then raise Exit
+      done
+    done;
+    true
+  with Exit -> false
+
+let problemcek = [|
+  [|Some 4; Some 8; Some 3; Some 9; Some 2; Some 1; Some 6; Some 5; Some 7|];
+  [|Some 9; Some 6; Some 7; Some 3; None  ; Some 5; Some 8; Some 2; Some 1|];
+  [|Some 2; Some 5; Some 1; Some 8; Some 7; Some 6; Some 4; Some 9; Some 3|];
+  [|Some 5; Some 4; Some 8; Some 1; Some 3; Some 2; Some 9; Some 7; Some 6|];
+  [|Some 7; Some 2; Some 9; None  ; Some 6; Some 4; None  ; Some 3; Some 8|];
+  [|Some 1; Some 3; Some 6; Some 7; Some 9; Some 8; None  ; Some 4; Some 5|];
+  [|Some 3; Some 7; Some 2; Some 6; Some 8; Some 9; Some 5; Some 1; Some 4|];
+  [|Some 8; Some 1; Some 4; Some 2; Some 5; Some 3; Some 7; Some 6; Some 9|];
+  [|Some 6; Some 9; Some 5; Some 4; Some 1; Some 7; Some 3; Some 8; Some 2|]|]
+
+let resitev = [|
+  [|Some 4; Some 8; Some 3; Some 9; Some 2; Some 1; Some 6; Some 5; Some 7|];
+  [|Some 9; Some 6; Some 7; Some 3; Some 4; Some 5; Some 8; Some 2; Some 1|];
+  [|Some 2; Some 5; Some 1; Some 8; Some 7; Some 6; Some 4; Some 9; Some 3|];
+  [|Some 5; Some 4; Some 8; Some 1; Some 3; Some 2; Some 9; Some 7; Some 6|];
+  [|Some 7; Some 2; Some 9; Some 5; Some 6; Some 4; Some 1; Some 3; Some 8|];
+  [|Some 1; Some 3; Some 6; Some 7; Some 9; Some 8; Some 2; Some 4; Some 5|];
+  [|Some 3; Some 7; Some 2; Some 6; Some 8; Some 9; Some 5; Some 1; Some 4|];
+  [|Some 8; Some 1; Some 4; Some 2; Some 5; Some 3; Some 7; Some 6; Some 9|];
+  [|Some 6; Some 9; Some 5; Some 4; Some 1; Some 7; Some 3; Some 8; Some 2|]|]
+
+(* type mozno =
+  | Pravilen of int
+  | Mozni of int * (int array)
+
+let update_grid i j n grid =
+  for k = 0 to 8 do
+    match grid.(i).(k) with 
+    | Mozni (a, moznosti) -> (moznosti.(n-1) <- 0)
+    | Pravilen a -> ();
+    match grid.(k).(j) with 
+    | Mozni (a, moznosti) -> (moznosti.(n-1) <- 0)
+    | Pravilen a -> ();
+    match grid.().() with 
+    | Mozni (a, moznosti) -> (moznosti.(n-1) <- 0)
+    | Pravilen a -> ()
+  done
+
+let nova_moznosti (problem : int option grid) : mozno grid =
+  (*Naredi nov grid, kjer so celice ali pravilne ali vsebujejo vse moznosti in stevilo koliko moznosti je*)
+  (* let moznosti = Array.init 9 (fun i -> Array.init 9 (fun k -> match problem.(i).(k) with None -> Mozni (9, (Array.init 9 (fun k -> k + 1))) | Some n -> Pravilen n)) in *)
+  let moznosti = Array.init 9 (fun i -> Array.init 9 (fun k -> Mozni (9, (Array.init 9 (fun k -> k + 1))))) in
+  for i = 0 to 8 do
+    for j = 0 to 8 do
+      if problem.(i).(j) != None then
+        update_grid i j (Option.get problem.(i).(j)) moznosti
+    done
+  done;
+  moznosti *)
--- domaca-naloga/solver.ml
+++ domaca-naloga/solver.ml
@@ -2,7 +2,21 @@ type available = { loc : int * int; possible : int list }
 
 (* TODO: tip stanja ustrezno popravite, saj boste med reševanjem zaradi učinkovitosti
    želeli imeti še kakšno dodatno informacijo *)
-type state = { problem : Model.problem; current_grid : int option Model.grid }
+type state = { problem : Model.problem; current_grid : int option Model.grid; possibilities : int list Model.grid }
+
+let rec print_list = function 
+[] -> ()
+| e::l -> print_int e ; print_string " " ; print_list l
+
+let int_list_to_string_fancy lst =
+  "[" ^ ( List.fold_left( fun acc x -> acc ^ string_of_int x ^ ";" ) "" lst) ^ "]"
+
+
+let add_to_grid i j n grid = 
+  Array.init 9 (fun a -> Array.init 9 (fun b -> if a = i && b = j then Some n else grid.(a).(b)))
+
+
+let problemcek = Model.problemcek
 
 let print_state (state : state) : unit =
   Model.print_grid
@@ -11,8 +25,33 @@ let print_state (state : state) : unit =
 
 type response = Solved of Model.solution | Unsolved of state | Fail of state
 
+let update_possibilities i j n sudoku = 
+  (* V sudoku doda n kot edino možnost v celici na (i, j)-tem mestu ter iz celic v vrstici, stolpcu in škatli odstrani n kot možnost*)
+  sudoku.(i).(j) <- [];
+  for k = 0 to 8 do
+    (*Odstrani iz vrstice in stolpca*) 
+    if k != i then sudoku.(k).(j) <- List.filter (fun a -> if a = n then false else true) sudoku.(k).(j);
+    if k != j then sudoku.(i).(k) <- List.filter (fun a -> if a = n then false else true) sudoku.(i).(k);
+    (*Odstrani iz box-a*)
+    if i != (3 * (i / 3) + (k mod 3)) || j != (3 * (j / 3) + (k / 3)) then 
+      sudoku.(3 * (i / 3) + (k mod 3)).(3 * (j / 3) + (k / 3)) <-  
+        List.filter (fun a -> if a = n then false else true) sudoku.(3 * (i / 3) + (k mod 3)).(3 * (j / 3) + (k / 3))
+  done
+
+let new_possibilities (problem : int option Model.grid) =
+  let vse_mozne = Array.init 9 (fun i -> Array.init 9 (fun j -> (List.init 9 (fun a -> a+1)))) 
+  (*Naredi sudoku kjer so v vsaki celici vse možnosti (tipa available grid)*)
+  in 
+  for i = 0 to 8 do
+    for j = 0 to 8 do 
+    if problem.(i).(j) != None then update_possibilities i j (Option.get problem.(i).(j)) vse_mozne
+    done 
+  done;
+  vse_mozne
+
 let initialize_state (problem : Model.problem) : state =
-  { current_grid = Model.copy_grid problem.initial_grid; problem }
+  (* Model.print_grid (int_list_to_string_fancy) (new_possibilities problem.initial_grid); *)
+  { current_grid = Model.copy_grid problem.initial_grid; problem ; possibilities = new_possibilities problem.initial_grid}
 
 let validate_state (state : state) : response =
   let unsolved =
@@ -21,27 +60,65 @@ let validate_state (state : state) : response =
   if unsolved then Unsolved state
   else
     (* Option.get ne bo sprožil izjeme, ker so vse vrednosti v mreži oblike Some x *)
-    let solution = Model.map_grid Option.get state.current_grid in
-    if Model.is_valid_solution state.problem solution then Solved solution
-    else Fail state
+    let solution = state.current_grid in
+    if Model.is_valid_solution (state.problem.initial_grid) (state.current_grid) then Solved (Model.map_grid Option.get solution) 
+      else Fail state 
 
-let branch_state (state : state) : (state * state) option =
-  (* TODO: Pripravite funkcijo, ki v trenutnem stanju poišče hipotezo, glede katere
-     se je treba odločiti. Če ta obstaja, stanje razveji na dve stanji:
+let rec print_list = function 
+  [] -> ()
+  | e::l -> print_int e ; print_string " " ; print_list l
+
+let prvi = function
+  |[] -> failwith "Prekratek seznam"
+  |x :: xs -> x
+
+let najdi_hipotezo moznosti =
+  (* print_string "iscemo hipotezo "; *)
+  let a, b, min = ref 9, ref 9, ref 9 in
+  for i = 0 to 8 do 
+    for j = 0 to 8 do 
+      if (moznosti.(i).(j) != []) && (List.length moznosti.(i).(j) < !min) then begin a := i; b:= j; min := (List.length moznosti.(i).(j)) end(*print_int !a; print_int !b;*)
+    done
+  done;
+  !a, !b
+
+  let branch_state (state : state) : (state * state) option =
+      (* print_string "branchamo "; *)
+    (* TODO: Pripravite funkcijo, ki v trenutnem stanju poišče hipotezo, glede katere
+    se je treba odločiti. Če ta obstaja, stanje razveji na dve stanji:
      v prvem predpostavi, da hipoteza velja, v drugem pa ravno obratno.
      Če bo vaš algoritem najprej poizkusil prvo možnost, vam morda pri drugi
      za začetek ni treba zapravljati preveč časa, saj ne bo nujno prišla v poštev. *)
-  failwith "TODO"
+    let i, j = najdi_hipotezo state.possibilities in
+    if not (i = 9) then begin
+      let n = prvi (state.possibilities.(i).(j)) in
+      let stare_moznosti = Model.copy_grid state.possibilities in
+      let nove_moznosti = Model.copy_grid state.possibilities in 
+      let nov_grid = add_to_grid i j n state.current_grid in
+      update_possibilities i j n nove_moznosti;
+      stare_moznosti.(i).(j) <- List.filter ((!=) n) state.possibilities.(i).(j);
+      Some ({problem = state.problem; current_grid = nov_grid; possibilities = nove_moznosti},
+       {problem = state.problem; current_grid = Model.copy_grid state.current_grid; possibilities = stare_moznosti}) end
+    else None
 
-(* pogledamo, če trenutno stanje vodi do rešitve *)
 let rec solve_state (state : state) =
+  (* print_string "rešujemo "; *)
   (* uveljavimo trenutne omejitve in pogledamo, kam smo prišli *)
   (* TODO: na tej točki je stanje smiselno počistiti in zožiti možne rešitve *)
+  let state = ref state in
+  for i = 0 to 8 do 
+    for j = 0 to 8 do 
+      if List.length !state.possibilities.(i).(j) = 1 then
+       state := {problem = !state.problem; current_grid = (add_to_grid i j (prvi !state.possibilities.(i).(j) ) !state.current_grid); possibilities = (new_possibilities (add_to_grid i j (prvi !state.possibilities.(i).(j) ) !state.current_grid))};
+    done
+  done;
+  (* print_state !state; *)
+  let state = !state in
   match validate_state state with
   | Solved solution ->
       (* če smo našli rešitev, končamo *)
       Some solution
-  | Fail fail ->
+  | Fail fail -> print_string "fail";
       (* prav tako končamo, če smo odkrili, da rešitev ni *)
       None
   | Unsolved state' ->
@@ -49,20 +126,21 @@ let rec solve_state (state : state) =
       explore_state state'
 
 and explore_state (state : state) =
+    (* print_string "raziskujemo "; *)
   (* pri raziskovanju najprej pogledamo, ali lahko trenutno stanje razvejimo *)
   match branch_state state with
-  | None ->
+  | None -> 
       (* če stanja ne moremo razvejiti, ga ne moremo raziskati *)
-      None
+        None
   | Some (st1, st2) -> (
       (* če stanje lahko razvejimo na dve možnosti, poizkusimo prvo *)
       match solve_state st1 with
       | Some solution ->
           (* če prva možnost vodi do rešitve, do nje vodi tudi prvotno stanje *)
           Some solution
-      | None ->
+      | None -> 
           (* če prva možnost ne vodi do rešitve, raziščemo še drugo možnost *)
           solve_state st2 )
 
 let solve_problem (problem : Model.problem) =
-  problem |> initialize_state |> solve_state
+  problem |> initialize_state |> solve_state |> (fun array -> match array with |None -> None |Some array -> Some (Model.map_grid (fun b -> Some b) array))
